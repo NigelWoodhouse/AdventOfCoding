3ffb2830d8e932ba1db68bac80e12f444ccd2a24
Nigel Woodhouse <njwoodhouse@gmail.com>
2021-12-05 21:40:58 -0700

    Dec 4

---
+++ December2021/Dec 4/Dec4.1.py
@@ -0,0 +1,48 @@
+import numpy as np
+f = open('numbers_drawn.txt', 'r')
+numbers_drawn = f.readline().rstrip().split(',')
+print(numbers_drawn)
+
+f.close()
+with open("bingo_cards.txt") as bingo_card:
+    bingo_cards_temp = [line.split() for line in bingo_card]
+    bingo_cards_temp = [x for x in bingo_cards_temp if x]
+bingo_cards = bingo_cards_temp
+temp_matrix = []
+for i in range(len(bingo_cards_temp)):
+    temp_matrix.append(bingo_cards_temp[i])
+    if len(temp_matrix) % 5 == 0:
+        temp_matrix = np.transpose(temp_matrix).tolist()
+        for j in range(len(temp_matrix)):
+            bingo_cards.append(temp_matrix[j])
+        temp_matrix = []
+
+called_numbers = []
+result = False
+i = 0
+for i in range(len(numbers_drawn)):
+    if result == True:
+        break
+    called_numbers.append(numbers_drawn[i])
+    if len(called_numbers) >= 5 and result != True:
+        for j in range(len(bingo_cards)):
+            result =  all(elem in called_numbers for elem in bingo_cards[j])
+            # print(bingo_cards[j])
+            if result == True:
+                print(bingo_cards[j], called_numbers, numbers_drawn[i])
+                winning_numbers = called_numbers
+                print(winning_numbers)
+                break
+
+winning_board = ['19', '85', '36', '73', '71',
+'65', '62', '14', '52',  '3',
+'30', '83', '44', '41',  '5',
+'55', '15',  '0', '61', '95',
+'28', '13', '32', '31', '88']
+
+board_sum = sum(map(int, winning_board))
+for elem in winning_numbers:
+    if elem in winning_board and elem in winning_numbers:
+        board_sum -= int(elem)
+board_sum = board_sum * int(winning_numbers[-1])
+print(board_sum)
---
+++ December2021/Dec 4/Dec4.2.py
@@ -0,0 +1,54 @@
+import numpy as np
+from numpy.core.fromnumeric import transpose
+f = open('numbers_drawn.txt', 'r')
+numbers_drawn = f.readline().rstrip().split(',')
+# print(numbers_drawn)
+f.close()
+
+num_lines = sum(1 for line in open('bingo_cards.txt'))+1
+card_num = int(num_lines/6)
+losing_count = 0
+losing_board = []
+for card in range(card_num):
+    with open("bingo_cards.txt", 'r') as infile:
+        lines = [line for line in infile][card*6:card*6+6]
+        bingo_card = [line.split() for line in lines]
+        bingo_card = [x for x in bingo_card if x]
+        for elem in np.transpose(bingo_card).tolist():
+            bingo_card.append(elem)
+
+
+    called_numbers = []
+    result = False
+    count = 0
+    while result == False:
+        called_numbers.append(numbers_drawn[count])
+        count += 1
+        if count >= 5:
+            for j in range(len(bingo_card)):
+                result =  all(elem in called_numbers for elem in bingo_card[j])
+                if result == True and count > losing_count:
+                    losing_count = count
+                    losing_board = bingo_card[0:5]
+                    final_number = called_numbers[-1]
+                    losing_numbers = called_numbers
+                    called_numbers = []
+                    count = 0
+                    break
+                elif result == True and count < losing_count:
+                    break
+                else:
+                    pass
+
+print(losing_board)
+print(losing_numbers)
+board_sum = sum(sum([list( map(int,i) ) for i in losing_board], []))
+losing_board = list(np.concatenate(losing_board).flat)            
+
+for elem in losing_numbers:
+    if elem in losing_board and elem in losing_numbers:
+        board_sum -= int(elem)
+board_sum = board_sum * int(final_number)
+print(board_sum)    
+
+#16168
