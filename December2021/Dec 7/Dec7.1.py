c4aa99b30f6ae50d9ce854b7a6611d1784e132f8
Nigel Woodhouse <njwoodhouse@gmail.com>
2021-12-06 22:51:47 -0700

    .

--- December2021/Dec 7/Dec7.1.py
+++ December2021/Dec 7/Dec7.1.py
@@ -4,8 +4,6 @@ f = open("numbers.txt", "r")
 crabs = f.readline().split(",")
 crabs = list(map(int, crabs))
 
-# print(crabs)
-# print(statistics.median(crabs))
 med = statistics.median(crabs)
 fuel = 0
 for i in range(len(crabs)):
