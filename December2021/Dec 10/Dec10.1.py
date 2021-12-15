4451dd99d60eef8a019b55b94d3f464f47c75f86
Nigel Woodhouse <njwoodhouse@gmail.com>
2021-12-14 21:56:43 -0700

    switch cases exist in python!

--- December2021/Dec 10/Dec10.1.py
+++ December2021/Dec 10/Dec10.1.py
@@ -1,6 +1,5 @@
 f = open("numbers.txt", "r")
 lines = f.read().split("\n")
-print(lines)
 
 sum = 0
 
@@ -9,29 +8,30 @@ for element in range(len(lines)):
     opening_array = []
     closing_array = []
     for char in lines[element]:
-        if char == "(":
-            closing_array.append(")")
-        elif char == "[":
-            closing_array.append("]")
-        elif char == "{":
-            closing_array.append("}")
-        elif char == "<":
-            closing_array.append(">")
-        elif char == ")":
-            if closing_array[-1] != ")":
-                sum += 3
-            closing_array = closing_array[:-1]
-        elif char == "]":
-            if closing_array[-1] != "]":
-                sum += 57
-            closing_array = closing_array[:-1]
-        elif char == "}":
-            if closing_array[-1] != "}":
-                sum += 1197
-            closing_array = closing_array[:-1]
-        elif char == ">":
-            if closing_array[-1] != ">":
-                sum += 25137
-            closing_array = closing_array[:-1]
+        match char:
+            case "(":
+                closing_array.append(")")
+            case "[":
+                closing_array.append("]")
+            case "{":
+                closing_array.append("}")
+            case "<":
+                closing_array.append(">")
+            case ")":
+                if closing_array[-1] != ")":
+                    sum += 3
+                closing_array = closing_array[:-1]
+            case "]":
+                if closing_array[-1] != "]":
+                    sum += 57
+                closing_array = closing_array[:-1]
+            case "}":
+                if closing_array[-1] != "}":
+                    sum += 1197
+                closing_array = closing_array[:-1]
+            case ">":
+                if closing_array[-1] != ">":
+                    sum += 25137
+                closing_array = closing_array[:-1]
 print(sum)
 # 299793
--- December2021/Dec 10/Dec10.2.py
+++ December2021/Dec 10/Dec10.2.py
@@ -10,44 +10,46 @@ for element in range(len(lines)):
     opening_array = []
     closing_array = []
     for char in lines[element]:
-        if char == "(":
-            closing_array.append(")")
-        elif char == "[":
-            closing_array.append("]")
-        elif char == "{":
-            closing_array.append("}")
-        elif char == "<":
-            closing_array.append(">")
-        elif char == ")":
-            if closing_array[-1] != ")":
-                sum += 3
-            closing_array = closing_array[:-1]
-        elif char == "]":
-            if closing_array[-1] != "]":
-                sum += 57
-            closing_array = closing_array[:-1]
-        elif char == "}":
-            if closing_array[-1] != "}":
-                sum += 1197
-            closing_array = closing_array[:-1]
-        elif char == ">":
-            if closing_array[-1] != ">":
-                sum += 25137
-            closing_array = closing_array[:-1]
+        match char:
+            case "(":
+                closing_array.append(")")
+            case "[":
+                closing_array.append("]")
+            case "{":
+                closing_array.append("}")
+            case "<":
+                closing_array.append(">")
+            case ")":
+                if closing_array[-1] != ")":
+                    sum += 3
+                closing_array = closing_array[:-1]
+            case "]":
+                if closing_array[-1] != "]":
+                    sum += 57
+                closing_array = closing_array[:-1]
+            case "}":
+                if closing_array[-1] != "}":
+                    sum += 1197
+                closing_array = closing_array[:-1]
+            case ">":
+                if closing_array[-1] != ">":
+                    sum += 25137
+                closing_array = closing_array[:-1]
     if sum != 0:
         sum = 0
     else:
         sum2 = 0
         closing_array.reverse()
         for chars in closing_array:
-            if chars == ")":
-                sum2 = sum2 * 5 + 1
-            elif chars == "]":
-                sum2 = sum2 * 5 + 2
-            elif chars == "}":
-                sum2 = sum2 * 5 + 3
-            elif chars == ">":
-                sum2 = sum2 * 5 + 4
+            match chars:
+                case ")":
+                    sum2 = sum2 * 5 + 1
+                case "]":
+                    sum2 = sum2 * 5 + 2
+                case "}":
+                    sum2 = sum2 * 5 + 3
+                case ">":
+                    sum2 = sum2 * 5 + 4
         sum_list.append(sum2)
 print(statistics.median(sum_list))
 # 3654963618
