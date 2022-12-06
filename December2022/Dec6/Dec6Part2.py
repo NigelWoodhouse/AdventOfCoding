4f1026e26995260db74e7d6e74f49e7e286af65d
Nigel Woodhouse <njwoodhouse@gmail.com>
2022-12-05 22:26:09 -0700

    Dec 6

---
+++ December2022/Dec6/Dec6Part2.py
@@ -0,0 +1,13 @@
+file = open("input.txt", "r")
+line = file.readline()
+marker_len = 14
+character_bucket = []
+print(character_bucket)
+for index, char in enumerate(line):
+    character_bucket.append(char)
+    if len(character_bucket) >= marker_len+1:
+        del character_bucket[0]
+    if len(set(character_bucket)) == len(character_bucket) and len(character_bucket) == marker_len:
+        print(index+1)
+        break
+# 3551
